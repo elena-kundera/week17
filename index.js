6e47852165ab367aca74b2908b96b4122f4f4f12
Елена <elena@MacBook-Air-Elena-2.local>
2022-12-18 17:01:56 +0300

    Добавила задание с датами

--- index.js
+++ index.js
@@ -71,5 +71,42 @@ random.onclick = function () {
     average.innerHTML = "Среднее арифметическое: " + averageNum;
     sum.innerHTML = "Сумма: " + summedNums;
     multiplicated.innerHTML = "Произведение: " + mutipliedNums;
-
 }
+
+
+
+//2. Сделайте интерфейс для анонимных комментариев со спамфильтром.
+
+// 3. Напишите функцию `formatDate(date)`, форматирующую `date` по следующему принципу: 
+//     - Если спустя `date` прошло менее 1 секунды, вывести `"прямо сейчас"`.
+//     - В противном случае, если с `date` прошло меньше 1 минуты, вывести `"n сек. 
+//     назад"`.
+//     - В противном случае, если меньше часа, вывести `"m мин. назад"`.
+//     - В противном случае, полная дата в формате `"DD.MM.YY HH:mm"`. 
+//     А именно: `"день.месяц.год часы:минуты"`, всё в виде двух цифр, 
+//     т.е. `31.12.16 10:00`.
+
+
+function formatDate(date) {
+    const today = new Date();
+    const year = today.getFullYear();
+    const yearShort = year.toString().slice(2);
+
+    let first = new Date() - date;
+
+if (first < 1) {
+    return "прямо сейчас";
+}
+else if (first < 30*1000) {
+    return "30 секунд назад";
+}
+else if (first < 5 * 6 * 1000) {
+    return "5 минут назад";
+}
+else {
+    return `${today.getDate()}.${today.getMonth()+1}.${yearShort} ${today.getHours()}:${today.getMinutes()}`;
+}
+}
+
+
+
